bcde4ad2248cb7673653fcf15f333dd77d39a761
zc <huchen.zhao@convertlab.com>
2022-04-19 16:24:18 +0800

    测试reset-01

---
+++ src/main/java/com/memory/glowingmemory/test/TestReset.java
@@ -0,0 +1,10 @@
+package com.memory.glowingmemory.test;
+
+/**
+ * @author zc
+ */
+public class TestReset {
+    public static void main(String[] args) {
+        System.out.println("第一次提交01");
+    }
+}
