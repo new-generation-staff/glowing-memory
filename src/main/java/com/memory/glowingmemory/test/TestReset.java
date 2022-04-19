ce92432ad41f05daee13d80f3a23d32a30ac910a
zc <huchen.zhao@convertlab.com>
2022-04-19 16:31:08 +0800

    测试reset-04

--- src/main/java/com/memory/glowingmemory/test/TestReset.java
+++ src/main/java/com/memory/glowingmemory/test/TestReset.java
@@ -7,5 +7,6 @@ public class TestReset {
     public static void main(String[] args) {
         System.out.println("第一次提交01");
         System.out.println("第一次提交02");
+        System.out.println("第三次提交01");
     }
 }
