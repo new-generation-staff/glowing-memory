5164d74fb2769a5eb18f4f3ee7c48e94035849db
zc <huchen.zhao@convertlab.com>
2022-04-19 16:34:33 +0800

    第五次提交01

--- src/main/java/com/memory/glowingmemory/test/TestReset.java
+++ src/main/java/com/memory/glowingmemory/test/TestReset.java
@@ -4,6 +4,7 @@ package com.memory.glowingmemory.test;
  * @author zc
  */
 public class TestReset {
+//    revert commit时
     public static void main(String[] args) {
         System.out.println("第一次提交01");
         System.out.println("第一次提交02");
