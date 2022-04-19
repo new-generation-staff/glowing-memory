cc657a6df9263c968236d30b645550bf391e7e12
zc <huchen.zhao@convertlab.com>
2022-04-19 16:43:42 +0800

    git revert 总结

--- src/main/java/com/memory/glowingmemory/test/TestRevert.java
+++ src/main/java/com/memory/glowingmemory/test/TestRevert.java
@@ -3,8 +3,8 @@ package com.memory.glowingmemory.test;
 /**
  * @author zc
  */
-public class TestReset {
-//    revert commit时
+public class TestRevert {
+    // revert commit 为撤销提交, 回滚到选中的提交 上次提交后的样子(即鼠标在【第13次提交】点击revert commit时，回滚到【第12次提交】时，此时可以rollback)
     public static void main(String[] args) {
         System.out.println("第一次提交01");
         System.out.println("第一次提交02");
